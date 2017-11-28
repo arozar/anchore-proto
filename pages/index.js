f95318d92b88a30048239975fcbce71a693b071e
Andrew <andrew_de_rozario@hotmail.com>
2017-11-28 16:39:44 +0000

    removing crap from index page

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import ImageTable from '../components/imagetable'
 
 class ImagePage extends React.Component {
   static async getInitialProps ({ store, isServer }) {
-    
+
     return { isServer }
   }
 
@@ -27,7 +27,7 @@ class ImagePage extends React.Component {
   }
 
   displayVulerabilities(id) {
-    Router.pushRoute('vuln', { id });    
+    Router.pushRoute('vuln', { id });
   }
   render () {
 
@@ -59,35 +59,3 @@ const mapStateToProps = ({ images, ajaxStatus }) => {
 }
 
 export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(ImagePage);
-
-const range = len => {
-  const arr = [];
-  for (let i = 0; i < len; i++) {
-    arr.push(i);
-  }
-  return arr;
-};
-
-const newPerson = () => {
-  const statusChance = Math.random();
-  return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? "relationship"
-        : statusChance > 0.33 ? "complicated" : "single"
-  };
-};
-
-export function makeData(len = 5553) {
-  return range(len).map(d => {
-    return {
-      ...newPerson(),
-      children: range(10).map(newPerson)
-    };
-  })
-}
